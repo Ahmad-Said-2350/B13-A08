536084721cc65cd4744cea319083127f67226635
Ahmad Said <mail.saydorrahman@gmail.com>
2026-05-03 21:32:38 +0600

    font famaly adeed

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,18 +1,14 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Urbanist } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { ToastContainer } from "react-toastify";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const UrbanistFont = Urbanist({
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+
 
 export const metadata = {
   title: "B13-A08",
@@ -23,7 +19,7 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${UrbanistFont.className}  h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
         <Navbar></Navbar>
--- src/app/page.js
+++ src/app/page.js
@@ -10,7 +10,7 @@ export default function Home() {
       <FeaturedAnimals></FeaturedAnimals>
       <QurbaniTips></QurbaniTips>
       
-x     
+ 
     </div>
   );
 }
